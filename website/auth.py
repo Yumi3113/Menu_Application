1eb6fe4651206207bbef4ef76ddcb1ac0b5dc14d
Louise <100814382+Louisewang248@users.noreply.github.com>
2022-06-14 12:30:02 +1200

    Signup form request restrictions

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,5 @@
 from re import template
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
@@ -13,4 +13,19 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
     return render_template("sign_up.html")
