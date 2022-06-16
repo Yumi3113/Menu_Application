9b58ae8482c1613c5abd196b375cfc12596d1d74
Louise <100814382+Louisewang248@users.noreply.github.com>
2022-06-17 09:17:48 +1200

    Adding users
    Identifies separate users
    adds database

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy
 DB_NAME = "database.db"
@@ -16,4 +18,21 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
+    from .models import User
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,29 @@
 from re import template
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
@@ -28,5 +47,6 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', catagory='error')
         else:
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash)
             flash('Account created', catagory='success')
     return render_template("sign_up.html")
