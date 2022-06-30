d27446de3f93d3ba8578d321529c445d4b8de607
Louise <100814382+Louisewang248@users.noreply.github.com>
2022-07-01 09:26:14 +1200

    Images
    Imported burger jpg and routes

--- website/views.py
+++ website/views.py
@@ -6,3 +6,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html", user=current_user)
+
+@views.route('item/<int:id>')
+def item(id):
+    return f"this is the item page for id {id}"
