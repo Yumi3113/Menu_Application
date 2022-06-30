9d70256e2104651ead6d3dfe1f15faed94967eab
Louise <100814382+Louisewang248@users.noreply.github.com>
2022-07-01 08:31:04 +1200

    Fixes

--- website/auth.py
+++ website/auth.py
@@ -31,31 +31,34 @@ def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
-@auth.route('/sign-up', methods=['GET','POST'])
+@auth.route('/sign_up', methods=['GET','POST'])
 def sign_up():
     if request.method == 'POST':
+        print ("post to signup")
         email = request.form.get('email')
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        print (first_name)
+
         user = User.query.filter_by(email=email).first()
         if user:
-            flash('Email already exists.',catagory='error')
+            flash('Email already exists.',category='error')
         elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
-            flash('first name must be greater than 1 character.', catagory='error')
+            flash('first name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            flash("Password didn\'t match.", catagory='error')
+            flash("Password didn\'t match.", category='error')
         elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', catagory='error')
+            flash('Password must be at least 7 characters.', category='error')
         else:
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-            flash('Account created', catagory='success')
+            flash('Account created', category='success')
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
