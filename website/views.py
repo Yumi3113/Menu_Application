fc33b2a4ed23c37fe1457a20ee4acc83ce9ea055
Louise <100814382+Louisewang248@users.noreply.github.com>
2022-08-04 14:43:14 +1200

    update

--- website/views.py
+++ website/views.py
@@ -1,11 +1,23 @@
+import sqlite3
+from tkinter import Menu
 from flask import Blueprint, render_template
 from flask_login import current_user
 
 views = Blueprint('views', __name__)
+DATABASE="website/database.db"
+
 
 @views.route('/')
 def home():
-    return render_template("home.html", user=current_user)
+    #connect and query
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM item"
+    cursor.execute(sql)
+    menu = cursor.fetchall()
+    print(menu)
+    db.close()
+    return render_template("home.html", user=current_user,menu=menu)
 
 @views.route('item/<int:id>')
 def item(id):
