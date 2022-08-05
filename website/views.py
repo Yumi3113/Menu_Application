de7ad29bf42c053cca20fe2ad7b420c203bfbe2a
Louise <100814382+Louisewang248@users.noreply.github.com>
2022-08-05 12:45:02 +1200

    update

--- website/views.py
+++ website/views.py
@@ -1,24 +1,31 @@
+from ast import arg
 import sqlite3
 from tkinter import Menu
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g
 from flask_login import current_user
 
 views = Blueprint('views', __name__)
 DATABASE="website/database.db"
 
+def query_db(query, args=(), one=False):
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    cursor.execute(query, args)
+    rv = cursor.fetchall()
+    cursor.close()
+    db.close()
+    #returns all the results unless one==True then it return just one result
+    #it also checks that something came back and sends None if it is blank
+    return (rv[0] if rv else None) if one else rv
 
 @views.route('/')
 def home():
     #connect and query
-    db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
     sql = "SELECT * FROM item"
-    cursor.execute(sql)
-    menu = cursor.fetchall()
-    print(menu)
-    db.close()
+    menu = query_db(sql)
     return render_template("home.html", user=current_user,menu=menu)
 
 @views.route('item/<int:id>')
 def item(id):
+    
     return f"this is the item page for id {id}"
